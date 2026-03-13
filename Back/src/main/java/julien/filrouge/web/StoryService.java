f15ce6bbed4bfcfaab7dde97af59b41143168641
Julien68480 <julien.simon68@gmail.com>
2026-03-13 11:00:35 +0100

    rajout POST

--- Back/src/main/java/julien/filrouge/web/StoryService.java
+++ Back/src/main/java/julien/filrouge/web/StoryService.java
@@ -1,5 +1,6 @@
 package julien.filrouge.web;
 
+import julien.filrouge.dto.StoryDto;
 import julien.filrouge.histoire.Story;
 import org.springframework.stereotype.Service;
 import julien.filrouge.web.dao.StoryDao;
@@ -18,5 +19,16 @@ public class StoryService {
 
     public Story findById(Long id){
         return storyDao.findById(id);
+    }
+
+    private Story construireStory(StoryDto dto){
+
+        return new Story(dto.getId(), dto.getTitre(), dto.getDescription(), dto.getDifficultyLevel(), dto.getCoverImage());
+    }
+
+    public Story save(StoryDto dto) {
+
+        return storyDao.save(construireStory(dto));
+
     }
 }
--- Back/src/main/java/julien/filrouge/web/controller/StoryControler.java
+++ Back/src/main/java/julien/filrouge/web/controller/StoryControler.java
@@ -1,11 +1,17 @@
 package julien.filrouge.web.controller;
 
+import julien.filrouge.dto.ShapeDto;
+import julien.filrouge.dto.StoryDto;
+import julien.filrouge.forme.mesformes.Shape;
 import julien.filrouge.histoire.Story;
 import julien.filrouge.web.StoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class StoryControler {
@@ -24,4 +30,21 @@ public class StoryControler {
         return storyService.findById(id);
     }
 
+    @PostMapping("/storys")
+    public ResponseEntity<Void> ajouterStory(@RequestBody StoryDto dto) {
+
+        Story saved = storyService.save(dto);
+
+        if (Objects.isNull(saved)) {
+            return ResponseEntity.noContent().build(); // 204 si échec
+        }
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()       // part de la requete actuelle
+                .path("/{id}")              // dit que ce sera sous /id qu'il sera créer
+                .buildAndExpand(saved.getId()) // récupére l'ID du POST
+                .toUri();
+
+        return ResponseEntity.created(uri).build(); // on retourne la réponse (le lien de l'objet) dans le cas de la création d'un objet 201
+    }
 }
